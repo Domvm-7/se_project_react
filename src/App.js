32e28ea7ce9e42272e4bf126f83e886132d37705
DominicMartinez <domvm.7@gmail.com>
2024-01-23 17:13:16 -0800

    Part5.0 complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header/Header";
+import WeatherCard from "./WeatherCard/WeatherCard";
 
 function App() {
   return (
     <div>
       <Header />
       <main className="main">
-        <section className="weather" id="weather">
-          <div className="weather__info">75F</div>
-          <img className="weather__image" src="/images/day/sunny.svg" />
-        </section>
+        <WeatherCard day={false} type="cloudy" />
         <section id="card-section">Card Section</section>
       </main>
     </div>
---
+++ src/WeatherCard/WeatherCard.js
@@ -0,0 +1,34 @@
+const weatherOptions = [
+  { url: "/images/day/sunny.svg", day: true, type: "sunny" },
+  { url: "/images/day/cloudy.svg", day: true, type: "cloudy" },
+  { url: "/images/day/fog.svg", day: true, type: "fog" },
+  { url: "/images/day/rain.svg", day: true, type: "rain" },
+  { url: "/images/day/snow.svg", day: true, type: "snow" },
+  { url: "/images/day/storm.svg", day: true, type: "storm" },
+  { url: "/images/night/sunny.svg", day: false, type: "sunny" },
+  { url: "/images/night/cloudy.svg", day: false, type: "cloudy" },
+  { url: "/images/night/fog.svg", day: false, type: "fog" },
+  { url: "/images/night/rain.svg", day: false, type: "rain" },
+  { url: "/images/night/snow.svg", day: false, type: "snow" },
+  { url: "/images/night/storm.svg", day: false, type: "storm" },
+];
+
+const WeatherCard = ({ day, type }) => {
+  console.log("weather card");
+  const imageSrc = weatherOptions.filter((i) => {
+    console.log(i);
+    return i.day === day && i.type === type;
+  });
+  console.log(imageSrc);
+  console.log(imageSrc[0].url);
+
+  const imageSrcUrl = imageSrc[0].url || "";
+  return (
+    <section className="weather" id="weather">
+      <div className="weather__info">65F</div>
+      <img className="weather__image" src={imageSrcUrl} />
+    </section>
+  );
+};
+
+export default WeatherCard;
