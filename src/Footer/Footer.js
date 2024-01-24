4819f82a02bc555b69dbc33916e9cc3d54a8687e
DominicMartinez <domvm.7@gmail.com>
2024-01-23 19:40:57 -0800

    Part 8 complete

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  console.log("footer");
+  return (
+    <footer className="footer">
+      <div>Developed by Dominic Martinez</div>
+      <div>2024</div>
+    </footer>
+  );
+};
+
+export default Footer;
