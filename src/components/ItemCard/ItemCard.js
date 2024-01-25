7072d9914b4ea4a9657aa98a77ea188de884fe11
DominicMartinez <domvm.7@gmail.com>
2024-01-25 13:10:43 -0800

    Moved item titles inside of card

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -4,13 +4,13 @@ const ItemCard = ({ item, onSelectCard }) => {
   return (
     <div>
       <div>
+        <div className="card__name"> {item.name}</div>
         <img
           src={item.link}
           className="card__image"
           onClick={() => onSelectCard(item)}
         />
       </div>
-      <div className="card__name"> {item.name}</div>
     </div>
   );
 };
