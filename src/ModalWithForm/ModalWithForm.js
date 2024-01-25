2310883528e6e67e7bd61bfed390a4dc5613a876
DominicMartinez <domvm.7@gmail.com>
2024-01-24 16:20:24 -0800

    modal css adjusted

--- src/ModalWithForm/ModalWithForm.js
+++ src/ModalWithForm/ModalWithForm.js
@@ -19,7 +19,9 @@ const ModalWithForm = ({
         <h3 className="modal__title">{title}</h3>
         <form>
           {children}
-          <button type="submit">{buttonText}</button>
+          <button className="modal__submit-button" type="submit">
+            {buttonText}
+          </button>
         </form>
       </div>
     </div>
