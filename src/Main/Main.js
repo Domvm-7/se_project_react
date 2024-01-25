82f71a61b9694caeb9153c5c6d8dff3dc0744aa9
DominicMartinez <domvm.7@gmail.com>
2024-01-25 00:35:55 -0800

    components

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,6 +1,6 @@
 import { defaultClothingItems } from "../utils/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
-import ItemCard from "../ItemCard/ItemCard";
+import ItemCard from "../Components/ItemCard/ItemCard";
 import "./Main.css";
 import { useMemo } from "react";
 
