8837c394cd1d7ff990d501d0492ced0390740156
DominicMartinez <domvm.7@gmail.com>
2024-02-24 10:04:13 -0800

    According to the brief this component should render a list of items, not a single item

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./ClothesSection.css";
 
-const ClothesSection = ({ item, onSelectCard }) => (
-  <div>
-    {item && (
-      <>
-        <div className="card__name">{item.name}</div>
-        <img
-          src={item.link}
-          className="card__image"
-          onClick={() => onSelectCard(item)}
-          alt={item.name}
-        />
-      </>
-    )}
+const ClothesSection = ({ items = [], onSelectCard }) => (
+  <div className="clothes-section">
+    {items &&
+      items.map((item, index) => (
+        <div key={index} className="clothes-section__item">
+          <div className="card__name">{item.name}</div>
+          <img
+            src={item.link}
+            className="card__image"
+            onClick={() => onSelectCard(item)}
+            alt={item.name}
+          />
+        </div>
+      ))}
   </div>
 );
 
